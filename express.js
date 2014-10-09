fa07fb0b7001fb08d3db9281058d549a2518adff
Paul Kristoff <Paul.Kristoff@kristoffs.com>
2014-10-09 05:58:22 -0400

    Update copyright button

--- express.js
+++ express.js
@@ -6,6 +6,10 @@
     var exif = require('exiftool');
     var bodyParser = require('body-parser');
     var formidable = require('formidable');
+    var spawn = require('child_process').spawn;
+
+    var Testdata = 'Testdata';
+    var Originals = 'Originals';
 
 
     app.use(bodyParser.json());       // to support JSON-encoded bodies
@@ -43,20 +47,28 @@
     var xxx = function (filenames, nonFilenames, totalFiles, res) {
         return function () {
             if (filenames.length + nonFilenames.length >= totalFiles) {
-                console.log("finally done");
+//                console.log("finally done");
                 res.status(200).send(filenames);
             } else {
-                console.log("looping: " + filenames.length + ":" + nonFilenames.length + ":" + totalFiles);
+//                console.log("looping: " + filenames.length + ":" + nonFilenames.length + ":" + totalFiles);
                 setTimeout(xxx(filenames, nonFilenames, totalFiles, res), 500);
             }
         }
     };
 
+    function getTestdataPath(rootFolder, contestName){
+        return rootFolder + "/" + contestName + "/" + Testdata + "/";
+    }
+
+    function getOriginalsPath(rootFolder, contestName){
+        return rootFolder + "/" + contestName + "/" + Originals + "/";
+    }
+
     app.get('/contest', function (req, res) {
         var rootFolder = req.query.rootFolder;
         var contestName = req.query.name;
-        var dirPath = rootFolder + "/" + contestName + "/Originals/";
-        console.log("dirPath: " + dirPath);
+        var dirPath = getTestdataPath(rootFolder, contestName);
+//        console.log("dirPath: " + dirPath);
         try {
             if (fs.existsSync(dirPath)) {
                 var contestContent = fs.readdirSync(dirPath);
@@ -75,7 +87,7 @@
 
         var filenames = [];
         var nonFilenames = [];
-        console.log("files: " + contestContent);
+//        console.log("files: " + contestContent);
         contestContent.forEach(function (filename) {
             var filenameSplit = filename.split(".");
             if (filenameSplit.length === 2 && (filenameSplit[1] === "jpg" || filenameSplit[1] === "JPG" || filenameSplit[1] === "jpeg" || filenameSplit[1] === "JPEG")) {
@@ -132,14 +144,14 @@
             }
         });
         form.on('file', function(field, file) {
-            console.log(file.name);
+//            console.log(file.name);
             files.push([field, file]);
         });
         form.on('end', function() {
-            console.log('done');
+//            console.log('done');
             var dirPath = rootFolder + "/" + contestName + "/";
-            var dirPathOriginals = dirPath + "Originals/";
-            var dirPathTestdata = dirPath + "Testdata/";
+            var dirPathOriginals = getOriginalsPath(rootFolder, contestName);
+            var dirPathTestdata = getTestdataPath(rootFolder, contestName);
             files.forEach(function (entry){
                 var file = entry[1];
                 var fn = file.name;
@@ -156,7 +168,7 @@
 //        var rootFolder = req.body.rootFolder;
 //        var contestName = req.body.name;
 //        var filenames = req.body.filenames;
-//        var dirPath = rootFolder + "/" + contestName + "/Originals/";
+//        var dirPath = getOriginalsPath(rootFolder, contestName);
 //        try {
 //            if (!fs.existsSync(dirPath)) {
 //                res.status(500).send(dirPath + " does not exist");
@@ -187,43 +199,36 @@
         }
     });
 
-    app.post('/setTitle', function (req, res){
-
-
-        var dirPath = rootFolder + "/" + contestName + "/";
-
-
-
-
-        var spawn = require('child_process').spawn;
+    app.post('/setCopyright', function (req, res){
+        var rootFolder = req.body.rootFolder;
+        var contestName = req.body.contestName;
+        var filename = req.body.filename;
+        var copyright = req.body.copyright;
+        var dirPath = getTestdataPath(rootFolder, contestName);
+        var error;
 
 
 
-        ls    = spawn('ls', ['-lh', '/usr']);
+        ls    = spawn('exiftool', ['-iptc:CopyrightNotice=\"' +  copyright + '\"', dirPath+filename]);
 
         ls.stdout.on('data', function (data) {
-            console.log('stdout: ' + data);
+//            console.log('stdout: ' + data);
         });
 
         ls.stderr.on('data', function (data) {
-            console.log('stderr: ' + data);
+            error = ("could not set copyright:  "  + data);
         });
 
         ls.on('close', function (code) {
-            console.log('child process exited with code ' + code);
+//            console.log('child process exited with code ' + code);
+            if (error){
+                res.status(500).send(error);
+            } else {
+                res.status(200).send(copyright);
+            }
         });
 
     });
-
-//            exiftool -imagesize -iptc:CopyrightNotice -iptc:caption-abstract -xmp:title -DateTimeOriginal -FileSize ./* > #{directory_info.exiftool_info_file}
-
-// date = @meta_data["Date/Time Original"]
-// year = (date.nil? or date.empty?) ? "2014" : date.split(":")[0]
-// copyright = "©#{year} #{@personName}"
-// #      puts "  copyright: #{copyright}"
-// xxx = "exiftool -iptc:CopyrightNotice=\"#{copyright}\" #{@tempFilename}"
-// #     puts "   copyright: '#{xxx}'"
-// system xxx
 
 
     app.post('/createContest', function (req, res) {
@@ -242,8 +247,8 @@
                 }
             } else {
                 fs.mkdirSync(dirPath);
-                fs.mkdirSync(dirPath + "Originals/");
-                fs.mkdirSync(dirPath + "Testdata/");
+                fs.mkdirSync(dirPath +  + "/' + Originals + '/");
+                fs.mkdirSync(dirPath +  + "/' + Testdata + '/");
                 contestContent = [];
             }
             res.status(200).send(contestContent);
